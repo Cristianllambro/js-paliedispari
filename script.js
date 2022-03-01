e2159f8e4c1dd2229c29efc8b6fbf033592e6c33
cristianllambro <llambro.cristian1996@gmail.comm>
2022-03-01 14:26:18 +0100

    resolve problem 2

---
+++ script.js
@@ -0,0 +1,49 @@
+// Esercizio parola palindroma
+
+/*
+const check = document.getElementById('check');
+const wordUser = prompt('Inserisci una parola:');
+let wordReverseUser = wordReverse(wordUser);
+
+function wordReverse (n) {
+    return n.split('').reverse().join('');
+}
+
+if (wordUser == wordReverseUser) {
+    check.innerHTML = 'La parola è palindroma';
+} else {
+    check.innerHTML = 'La parola non è palindroma';
+}
+*/
+
+
+
+
+
+// Esercizio pari e dispari 
+
+let oddOrEven = prompt('Scegli pari o dispari:');
+let numberUser = parseInt(prompt('Scegli un numero da 1 a 5:'));
+const randomNum = numberRandom(1, 5);
+
+let sum = numberUser + randomNum;
+
+
+function numberRandom (min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function numberEven(numberE) {
+    if (numberE % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+if (numberEven(sum)) {
+    check.innerHTML = 'pari';
+} else {
+    check.innerHTML = 'dispari'
+}
+
